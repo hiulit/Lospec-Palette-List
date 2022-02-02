8b27ea1dbb8fd9ff94d927de54dc1afffc89ba04
hiulit <hiulit@gmail.com>
2022-02-02 21:27:38 +0100

    Separate plugin from app

--- addons/lospec_palette_list/main_window.gd
+++ addons/lospec_palette_list/main_window.gd
@@ -1,31 +1,27 @@
 tool
 extends Control
 
-const CONFIG_FILE := "user://godot_lospec_settings.cfg"
-
 var config := ConfigFile.new()
+var config_file := "user://plugin_settings.cfg"
 
 var base_url := "https://lospec-api.vercel.app/api"
 
 var query_params := {
-	"filter_type": "any",
-	"number": 8,
-	"page": 0,
-	"tag": "",
-	"sorting_type": "default"
+	"filter_type": "any", "number": 8, "page": 0, "tag": "", "sorting_type": "default"
 }
 
-var url
-var query_string
+var url: String
+var query_string: String
 
 var current_pressed_filter_type_button: Button
 var current_pressed_sort_button: Button
 
-var current_download_path: String
+var default_download_path := "res://"
+var current_download_path: String setget _set_current_download_path
 
 var slider_min_value := 2
 var slider_timer_on := false
-var slider_editing_value
+var slider_editing_value: int
 
 var total_count
 var max_pages
@@ -35,34 +31,37 @@ var base_color_rect_size = 8
 
 var debug_mode := false
 
-onready var button_group_filter_type := preload("res://addons/lospec_palette_list/resources/button_group_filter_type.tres")
-onready var button_group_sort := preload("res://addons/lospec_palette_list/resources/button_group_sort.tres")
-onready var palette_item_container := preload("res://addons/lospec_palette_list/components/palette_item_container/palette_item_container.tscn")
+onready var button_group_filter_type := preload(
+	"res://addons/lospec_palette_list/resources/button_group_filter_type.tres"
+)
+onready var button_group_sort := preload(
+	"res://addons/lospec_palette_list/resources/button_group_sort.tres"
+)
+onready var palette_item_container := preload(
+	"res://addons/lospec_palette_list/components/palette_item_container/palette_item_container.tscn"
+)
 
-onready var http_request := $HTTPRequest
-onready var filter_type_buttons_container := $MainContainer/ColorSelectorContainer/HBoxContainer/FilterTypeButtonsContainer
-onready var sorting_buttons_container := $MainContainer/SearchAndSortContainer/SortingContainer/SortingButtonsContainer
-onready var slider := $MainContainer/ColorSelectorContainer/HBoxContainer/SliderContainer/Slider
-onready var slider_line_edit := $MainContainer/ColorSelectorContainer/HBoxContainer/SliderContainer/SliderLineEdit
-onready var search_by_tag_line_edit := $MainContainer/SearchAndSortContainer/SearchByTagContainer/SearchByTagLineEdit
-onready var download_path_line_edit := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/DownloadPathContainer/DownloadPathLineEdit
 onready var download_path_button := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/DownloadPathContainer/DownloadPathButton
 onready var download_path_file_dialog := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/DownloadPathContainer/DownloadPathFileDialog
-onready var results_label := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/ResultsWrapper/ResultsContainer/ResultsLabel
+onready var download_path_line_edit := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/DownloadPathContainer/DownloadPathLineEdit
+onready var filter_type_buttons_container := $MainContainer/ColorSelectorContainer/HBoxContainer/FilterTypeButtonsContainer
+onready var grid_container := $MainContainer/PalettesContainer/ScrollContainer/GridContainer
+onready var http_request := $HTTPRequest
+onready var next_button := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/ResultsWrapper/PaginationContainer/NextButton
 onready var overlay_container := $MainContainer/PalettesContainer/OverlayContainer
 onready var overlay_container_label := $MainContainer/PalettesContainer/OverlayContainer/OverlayLabel
 onready var previous_button := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/ResultsWrapper/PaginationContainer/PreviousButton
-onready var next_button := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/ResultsWrapper/PaginationContainer/NextButton
-onready var grid_container := $MainContainer/PalettesContainer/ScrollContainer/GridContainer
+onready var results_label := $MainContainer/ResultsAndDowloadPathContainer/ResultsAndDownloadPathWrapper/ResultsWrapper/ResultsContainer/ResultsLabel
+onready var search_by_tag_line_edit := $MainContainer/SearchAndSortContainer/SearchByTagContainer/SearchByTagLineEdit
+onready var slider := $MainContainer/ColorSelectorContainer/HBoxContainer/SliderContainer/Slider
+onready var slider_line_edit := $MainContainer/ColorSelectorContainer/HBoxContainer/SliderContainer/SliderLineEdit
+onready var sorting_buttons_container := $MainContainer/SearchAndSortContainer/SortingContainer/SortingButtonsContainer
 
 
 func _ready():
-	OS.set_window_title("Lospec Palette List")
-
-	set_query_string()
-	set_url()
-
 	# Connect signals.
+	connect("resized", self, "_on_main_window_resized")
+
 	http_request.connect("request_completed", self, "_on_http_request_completed")
 
 	for button in filter_type_buttons_container.get_children():
@@ -78,36 +77,27 @@ func _ready():
 	slider.connect("value_changed", self, "_on_slider_value_changed")
 	slider_line_edit.connect("text_entered", self, "_on_slider_line_edit_text_entered")
 
-	search_by_tag_line_edit.connect("text_entered", self, "_on_search_by_tag_line_edit_text_entered")
-	search_by_tag_line_edit.connect("text_changed", self, "_on_search_by_tag_line_edit_text_changed")
+	search_by_tag_line_edit.connect(
+		"text_entered", self, "_on_search_by_tag_line_edit_text_entered"
+	)
+	search_by_tag_line_edit.connect(
+		"text_changed", self, "_on_search_by_tag_line_edit_text_changed"
+	)
 
 	previous_button.connect("pressed", self, "_on_previous_button_pressed")
 	next_button.connect("pressed", self, "_on_next_button_pressed")
 
 	download_path_button.connect("pressed", self, "_on_download_path_button_pressed")
-	download_path_file_dialog.connect("dir_selected", self, "on_download_path_file_dialog_dir_selected")
+	download_path_file_dialog.connect(
+		"dir_selected", self, "on_download_path_file_dialog_dir_selected"
+	)
 
 	# Create config file.
-	create_config_file()
-
-	# Load config file.
-	config.load(CONFIG_FILE)
-
-	# Use global file system when exported.
-	if not Engine.editor_hint:
-		config.set_value(
-			"settings",
-			"download_path",
-			ProjectSettings.globalize_path(config.get_value("settings", "download_path"))
-		)
-
-	current_download_path = config.get_value("settings", "download_path")
-
-	if not Engine.editor_hint:
-		# Access file system.
-		download_path_file_dialog.access = 2
+	config_create()
 
 	# Set initial values.
+	self.current_download_path = config_get("settings", "download_path")
+
 	filter_type_buttons_container.get_child(0).pressed = true
 	sorting_buttons_container.get_child(0).pressed = true
 
@@ -116,34 +106,51 @@ func _ready():
 
 	results_label.text = "0 results"
 
-	download_path_line_edit.text = current_download_path
-
 	overlay_container.visible = false
 
-	if Engine.editor_hint:
-		base_color_rect_size *= 2
-		results_label.rect_min_size.x = 250
-	else:
-		results_label.rect_min_size.x = 100
-
+	# Make the HTTP call.
 	make_http_call()
 
 
-func create_config_file():
+func config_create() -> void:
 	var f = File.new()
 
-	if not f.file_exists(CONFIG_FILE):
-		config.set_value("settings", "download_path", "res://")
-		config.save(CONFIG_FILE)
+	if not f.file_exists(config_file):
+		config.set_value("settings", "download_path", default_download_path)
+		config.save(config_file)
+
+
+func config_get(section: String, key: String) -> String:
+	var error = config.load(config_file)
+	if error != OK:
+		push_error("ERROR: Couldn't load the config file!")
+
+	return config.get_value("settings", "download_path")
+
+
+func config_set(section: String, key: String, value: String) -> void:
+	var error = config.load(config_file)
+	if error != OK:
+		push_error("ERROR: Couldn't load the config file!")
+
+	config.set_value(section, key, value)
+	config.save(config_file)
 
 
 func set_query_string():
-	query_string = "?" \
-		+ "colorNumberFilterType=" + query_params.filter_type \
-		+ "&colorNumber=" + str(query_params.number) \
-		+ "&page=" + str(query_params.page) \
-		+ "&tag=" + query_params.tag \
-		+ "&sortingType=" + query_params.sorting_type
+	query_string = (
+		"?"
+		+ "colorNumberFilterType="
+		+ query_params.filter_type
+		+ "&colorNumber="
+		+ str(query_params.number)
+		+ "&page="
+		+ str(query_params.page)
+		+ "&tag="
+		+ query_params.tag
+		+ "&sortingType="
+		+ query_params.sorting_type
+	)
 
 
 func set_url():
@@ -179,7 +186,7 @@ func make_http_call():
 func editing_slider(new_value):
 	if not slider_timer_on:
 		slider_timer_on = true
-		yield(get_tree().create_timer(0.2), "timeout" )
+		yield(get_tree().create_timer(0.2), "timeout")
 		slider_timer_on = false
 
 		if slider_editing_value != new_value:
@@ -197,6 +204,19 @@ func editing_slider(new_value):
 			make_http_call()
 
 	slider_editing_value = new_value
+
+
+func _on_main_window_resized():
+	pass
+#	print(rect_size / 2)
+#	if rect_size.x / 2 < 700:
+#		grid_container.columns = 1
+#	elif rect_size.x / 2 < 1000:
+#		grid_container.columns = 2
+#	elif rect_size.x / 2 < 1300:
+#		grid_container.columns = 3
+#	else:
+#		grid_container.columns = 4
 
 
 func _on_http_request_completed(result, response_code, headers, body):
@@ -248,7 +268,7 @@ func _on_http_request_completed(result, response_code, headers, body):
 		else:
 			palette_item_instance.created_by.text = ""
 
-		if "tags" in response.palettes[i]:
+		if "tags" in response.palettes[i] and response.palettes[i].tags:
 			for tag in response.palettes[i].tags:
 				var tag_button = Button.new()
 
@@ -256,12 +276,9 @@ func _on_http_request_completed(result, response_code, headers, body):
 				tag_button.text = tag
 				tag_button.connect("pressed", self, "_on_tag_button_pressed", [tag_button.text])
 
-#				if tag != response.palettes[i].tags[response.palettes[i].tags.size() - 1]:
-#					button.text += ","
-
 				palette_item_instance.tags_list.add_child(tag_button)
 		else:
-			palette_item_instance.tags_list.text = ""
+			palette_item_instance.tags_label.text = ""
 
 		var palette_colors = []
 
@@ -301,13 +318,12 @@ func _on_http_request_completed(result, response_code, headers, body):
 
 			color_rect.rect_min_size = Vector2.ONE * (base_color_rect_size * ratio)
 			color_rect.rect_size = color_rect.rect_min_size
-			
+
 			palette_item_instance.colors_grid.add_child(color_rect)
 
 		palette_item_instance.palette_colors = palette_colors
 		palette_item_instance.slug = response.palettes[i].slug
 		palette_item_instance.current_download_path = current_download_path
-		palette_item_instance.base_url = base_url
 
 	results_label.text = str(total_count) + " results"
 
@@ -399,30 +415,27 @@ func _on_next_button_pressed():
 	make_http_call()
 
 
-func _on_download_path_button_pressed():
+func _on_download_path_button_pressed() -> void:
+	download_path_file_dialog.current_dir = current_download_path
 	download_path_file_dialog.popup_centered_ratio(0.5)
 
 
-func on_download_path_file_dialog_dir_selected(dir):
-	config.set_value("settings", "download_path", ProjectSettings.localize_path(dir))
-	config.save(CONFIG_FILE)
+func on_download_path_file_dialog_dir_selected(dir: String) -> void:
+	config_set("settings", "download_path", dir)
 
-	# Use global file system when exported.
-	if not Engine.editor_hint:
-		config.set_value(
-			"settings",
-			"download_path",
-			ProjectSettings.globalize_path(config.get_value("settings", "download_path"))
-		)
-
-	current_download_path = config.get_value("settings", "download_path")
-
-	download_path_line_edit.text = current_download_path
-
-	for item in get_tree().get_nodes_in_group("palette_item_container"):
-		item.current_download_path = current_download_path
+	self.current_download_path = config_get("settings", "download_path")
 
 
-func _on_tag_button_pressed(text):
+func _on_tag_button_pressed(text) -> void:
 	search_by_tag_line_edit.text = text
 	search_by_tag_line_edit.emit_signal("text_entered", text)
+
+
+func _set_current_download_path(new_value) -> void:
+	if new_value != current_download_path:
+		current_download_path = new_value
+
+		download_path_line_edit.text = current_download_path
+
+		for item in get_tree().get_nodes_in_group("palette_item_container"):
+			item.current_download_path = current_download_path
---
+++ app/app.gd
@@ -0,0 +1,36 @@
+extends Control
+
+var window := {
+	"width": ProjectSettings.get_setting("display/window/size/width"),
+	"height": ProjectSettings.get_setting("display/window/size/height"),
+}
+
+var main_window_instance: Node
+
+onready var main_window := preload("res://addons/lospec_palette_list/main_window.tscn")
+
+
+func _ready():
+	OS.set_window_title("Lospec Palette List")
+
+	if ProjectSettings.get_setting("display/window/dpi/allow_hidpi"):
+		get_tree().set_screen_stretch(
+			SceneTree.STRETCH_MODE_DISABLED,
+			SceneTree.STRETCH_ASPECT_IGNORE,
+			Vector2(window.width * 2, window.height * 2),
+			2
+		)
+
+		OS.set_window_size(Vector2(window.width * 2, window.height * 2))
+
+		OS.center_window()
+
+	main_window_instance = main_window.instance()
+
+	main_window_instance.config_file = "user://app_settings.cfg"
+
+	main_window_instance.default_download_path = OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP)
+
+	add_child(main_window_instance)
+
+	main_window_instance.download_path_file_dialog.access = 2
